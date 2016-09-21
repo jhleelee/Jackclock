51d120c3da07814d7a2feb65da41f8daf757ed82
jhleelee <jaehak.lee.1984@gmail.com>
2016-09-21 17:46:10 -0400

    Concept Change
    Jack - Alarm - AlarmOwner - AlarmOwnerMe
    
    Jack - Event - EventOwner - EventOwnerMe

--- app/src/main/java/com/jackleeentertainment/jackclock/object/Jack.java
+++ app/src/main/java/com/jackleeentertainment/jackclock/object/Jack.java
@@ -16,4 +16,37 @@ public class Jack {
     public Jack() {
         super();
     }
+
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
 }
