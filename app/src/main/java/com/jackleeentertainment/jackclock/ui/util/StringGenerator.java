cfbdeb5e925facb6471f5829a167774d6a72b2d9
jhleelee <jaehak.lee.1984@gmail.com>
2016-09-20 21:24:43 -0400

    initial Commit

--- app/src/main/java/com/jackleeentertainment/jackclock/ui/util/StringGenerator.java
+++ app/src/main/java/com/jackleeentertainment/jackclock/ui/util/StringGenerator.java
@@ -3,6 +3,7 @@ package com.jackleeentertainment.jackclock.ui.util;
 import android.content.Context;
 
 import com.jackleeentertainment.jackclock.R;
+import com.jackleeentertainment.jackclock.generalutil.J;
 
 /**
  * Created by Jacklee on 2016. 9. 10..
@@ -21,5 +22,16 @@ public class StringGenerator {
 
     }
 
+    public static String xSentMessage(String name, int msgNum, Context context) {
+
+        // get locale
+
+        if (msgNum==1){
+            return name + " " + JM.strById(R.string.sent, context) +" " + JM.strById(R.string.message, context);
+        } else {
+            return name + " " + JM.strById(R.string.sent, context) +" " + J.st(msgNum) + " " + JM.strById(R.string.message, context);
+        }
+
+    }
 
 }
